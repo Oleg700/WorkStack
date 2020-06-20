c2847f5924e3314e83ec27fea27b4dce487e453c
Oleg Aliyev <oleg_aliyev@epam.com>
2020-06-20 12:04:58 +0600

    added Swagger2

--- src/main/java/org/workstack/config/AppConfig.java
+++ src/main/java/org/workstack/config/AppConfig.java
@@ -7,8 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class AppConfig {
 
     @Bean
@@ -29,5 +35,14 @@ public class AppConfig {
         LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
         factory.setValidationMessageSource(messageSource());
         return factory;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
